49a4410b44f5e873a8320d4521e093ce632d8044
Talk-NerdyToMe <talknerdytome175@gmail.com>
2025-01-06 21:44:31 +0400

    Update state-locking.tf

--- january-2025/terraform/state-management/state-locking.tf
+++ january-2025/terraform/state-management/state-locking.tf
@@ -4,3 +4,7 @@ resource_group_name  = "tfstate"
 storage_account_name = "tfstate1234"
 container_name      = "tfstate"
 key                 = "prod.terraform.tfstate"
+Copy# Enable state locking
+lock_enabled = true
+}
+}
